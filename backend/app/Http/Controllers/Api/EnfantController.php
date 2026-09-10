c901262b2c7b201156b10802efa680b50e516c0d
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-10 22:22:34 +0100

    ajouter crud pour enfant

--- backend/app/Http/Controllers/Api/EnfantController.php
+++ backend/app/Http/Controllers/Api/EnfantController.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEnfantRequest;
 use App\Models\Enfant;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class EnfantController extends Controller
 {
+    use AuthorizesRequests;
     //
     public function index(Request $request)
     {
@@ -32,5 +34,39 @@ class EnfantController extends Controller
             'message' => 'Profil enfant crée avec succès',
             'enfant' => $enfant
         ], 201);
+    }
+
+    function show(Enfant $enfant)
+    {
+        $this->authorize('view', $enfant);
+
+        return response()->json([
+            'enfant' =>$enfant
+        ]);
+    }
+
+    public function update(StoreEnfantRequest $request, Enfant $enfant)
+    {
+        $this->authorize('update', $enfant);
+
+        $enfant->update([
+            'prenom' => $request->prenom,
+            'date_naissance' => $request->date_naissance,
+        ]);
+
+        return response()->json([
+            'message' => 'Profil enfant modifié avec succès',
+            'enfant' => $enfant
+        ]);
+    }
+
+    public function destroy(Enfant $enfant)
+    {
+        $this->authorize('delete', $enfant);
+        $enfant->delete();
+
+        return response()->json([
+            'message' => 'Profil enfant supprimé avec succès',
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EnfantController;
+use Laravel\Sanctum\Sanctum;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,6 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 
 Route::middleware('auth:sanctum')->post('/enfants',[EnfantController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/enfants', [EnfantController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/enfants/{enfant}', [EnfantController::class, 'show']);
+Route::middleware('auth:sanctum')->put('/enfants/{enfant}', [EnfantController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/enfants/{enfant}', [EnfantController::class, 'destroy']);
