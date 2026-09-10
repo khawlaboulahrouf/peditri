5c5e93a36be7468237fc3d2ed096eff8c93ff54b
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-10 21:15:52 +0100

    ajouter fonction cree enfant

---
+++ backend/app/Http/Controllers/Api/EnfantController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEnfantRequest;
+use App\Models\Enfant;
+use Illuminate\Http\Request;
+
+class EnfantController extends Controller
+{
+    //
+  
+
+    public function store(StoreEnfantRequest $request)
+    {
+        $enfant = Enfant::create([
+            'prenom' => $request->prenom,
+            'date_naissance' => $request->date_naissance,
+            'user_id' => $request->user()->id,
+        ]);
+
+        return response()->json([
+            'message' => 'Profil enfant crée avec succès',
+            'enfant' => $enfant
+        ], 201);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EnfantController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,5 @@ Route::post('/register',[AuthController::class, 'register']);
 
 Route::post('/login',[AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->post('/enfants',[EnfantController::class, 'store']);
