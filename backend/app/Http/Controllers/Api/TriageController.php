3b135618b19af666dbd36dd25bd187a1bc410620
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-14 15:09:58 +0100

    ajouter fonction store dans triage controller

--- backend/app/Http/Controllers/Api/TriageController.php
+++ backend/app/Http/Controllers/Api/TriageController.php
@@ -15,7 +15,14 @@ class TriageController extends Controller
 
     public function store(Request $request, Enfant $enfant)
     {
-        
+        $this->authorize('view' , $enfant);
+        $triage = Triage::create([
+            'enfant_id' => $enfant->id,
+            'date_debut' => now(),
+        ]);
+        return response()->json([
+            'message' => 'Triage démarré avec succès',
+            'triage' => $triage
+        ], 201);
     }
-
 }
