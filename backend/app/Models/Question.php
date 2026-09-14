b69a9050acb08d0940933cb820f5387b28fb8e14
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-14 13:18:11 +0100

    ajouter relation entre question et reponce

--- backend/app/Models/Question.php
+++ backend/app/Models/Question.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Question extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'titre',
+        'group_age',
+        'order',
+    ];
+
+    public function responses()
+    {
+        return $this->hasmany(Response::class);
+    }
 }
--- backend/app/Models/Response.php
+++ backend/app/Models/Response.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Response extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'triage_id',
+        'question_id',
+        'label',
+        'status',
+    ];
+
+    public function triage()
+    {
+        return $this->belongsTo(Triage::class);
+    }
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
