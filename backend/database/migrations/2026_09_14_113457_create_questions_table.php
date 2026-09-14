ad4c730b5401928facb3aaf3b752a3bcf516ef38
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-14 12:38:37 +0100

    ajouter model question

--- backend/database/migrations/2026_09_14_113457_create_questions_table.php
+++ backend/database/migrations/2026_09_14_113457_create_questions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('titre');
+            $table->string('groupe_age');
+            $table->integer('order');
             $table->timestamps();
         });
     }
