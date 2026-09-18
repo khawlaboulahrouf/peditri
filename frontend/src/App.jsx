c20067c59a518e25e8f222c90d377dd4302b04e8
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-09-18 10:58:31 +0100

    commencer la consomation d'api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Enfants from "./pages/Enfants";
+import Triage from "./pages/Triage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/enfants" element={<Enfants />} />
+        <Route path="/triage/:enfantId" element={<Triage/>}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Triage.jsx
+++ frontend/src/pages/Triage.jsx
@@ -1,12 +1,51 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function Triage() {
     const { enfantId} = useParams();
+    const [triage, setTriage] = useState(null);
+    const [question , setQuestion] = useState(null);
+    const [message , setMessage] = useState("");
+
+    const token = localStorage.getItem("token");
+    const commencerTriage = async () => {
+        try{ 
+            const response = await axios.post(
+                `http://127.0.0.1:8000/api/enfants/${enfantId}/triages`,
+                {},
+                {
+                    headers: {
+                        Authorization:`Bearer ${token}`,
+                        Accept: "application/json",
+                    },
+                }
+            );
+
+            setTriage(response.data.triage);
+            setQuestion(response.data.question);
+        }catch(error) {
+            setMessage("Erreur lors du démarrage du triage");
+        }
+    };
 
     return (
         <div>
             <h1>Triage</h1>
-            <p>Enfant sélectionné : {enfantId}</p>
+
+            {!triage && (
+                <button onClick={commencerTriage}>
+                    Démarrer le questionnaire
+                </button>
+            )}
+
+            {question && (
+                <div>
+                    <h2>{question.titre}</h2>
+                </div>
+            )}
+
+            {message && <p>{message}</p>}
         </div>
     );
 }
